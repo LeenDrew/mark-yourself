9884689382a8bea7d43a50c2b1f4b0b05883b601
IndoorGhoul <hermione1granger121@gmail.com>
2021-10-05 10:57:48 +0600

    fix

--- src/controllers/subGroup-controller.ts
+++ src/controllers/subGroup-controller.ts
@@ -1,11 +1,5 @@
 import SubGroup, { ISubGroup } from '../models/SubGroup';
 
-// export const createOne = async (subGroup: ISubGroup): Promise<ISubGroup | null> => {
-//   const sub = new SubGroup({ ...subGroup });
-//   const res = await sub.save();
-//   return res;
-// };
-
 export const createMany = async (subGroups: ISubGroup[]): Promise<ISubGroup[] | null> => {
   const res = await SubGroup.insertMany(subGroups);
   return res;
@@ -13,6 +7,6 @@ export const createMany = async (subGroups: ISubGroup[]): Promise<ISubGroup[] | 
 
 export const getManyByGroupName = async (groupName: string): Promise<ISubGroup[] | null> => {
   const groupNameRegExp = RegExp(`^${groupName}`, 'i');
-  const subGroups = await SubGroup.find({ name: groupNameRegExp });
+  const subGroups = await SubGroup.find({ subGroupName: groupNameRegExp });
   return subGroups;
 };
