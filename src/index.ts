25de77a7ff3eeb6f8924d4cae87f2d22e6b52cee
IndoorGhoul <hermione1granger121@gmail.com>
2021-10-09 22:46:30 +0600

    fix: start promise

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import mongoose from 'mongoose';
 import { vk } from './bot';
 import { config } from './config';
 
-(async () => {
-  try {
-    await mongoose.connect(config.MONGO_URI, {
+(() => {
+  mongoose
+    .connect(config.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    } as mongoose.ConnectOptions);
-    vk.updates
-      .start()
-      .then(() => console.log('Bot was started'))
-      .catch(console.error);
-  } catch (e) {
-    console.error(`mongo connect ERROR: ${e as string}`);
-  }
+    } as mongoose.ConnectOptions)
+    .catch(console.error);
+  vk.updates
+    .start()
+    .then(() => console.log('Bot was started'))
+    .catch(console.error);
 })();
