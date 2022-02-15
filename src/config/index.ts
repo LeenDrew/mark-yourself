70c999a0c17355733418e85ecf896b04a0ca4f27
IndoorGhoul <hermione1granger121@gmail.com>
2022-02-15 20:17:02 +0600

    fix: add config env check

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,31 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+enum Env {
+  VK_API_TOKEN = 'VK_API_TOKEN',
+  VK_API_LIMIT = 'VK_API_LIMIT',
+  VK_API_CALLBACK_RESPONSE = 'VK_API_CALLBACK_RESPONSE',
+  VK_GROUP_ID = 'VK_GROUP_ID',
+  MONGO_LOGIN = 'MONGO_LOGIN',
+  MONGO_PASSWORD = 'MONGO_PASSWORD',
+  MONGO_URI = 'MONGO_URI',
+}
+
+const checkEnv = (env: string) => {
+  if (!process.env[env]) {
+    throw new Error(`Check that '${env}' really exist in your .env file`);
+  }
+  return String(process.env[env]);
+};
+
 const initConfig = () => ({
-  VK_API_TOKEN: process.env.VK_API_TOKEN || '',
-  VK_API_LIMIT: parseInt(process.env.VK_API_LIMIT ?? '', 10),
-  VK_API_CALLBACK_RESPONSE: process.env.VK_API_CALLBACK_RESPONSE || '',
-  VK_GROUP_ID: parseInt(process.env.VK_GROUP_ID ?? '', 10),
-  MONGO_LOGIN: process.env.MONGO_LOGIN || '',
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD || '',
-  MONGO_URI: process.env.MONGO_URI || '',
+  VK_API_TOKEN: checkEnv(Env.VK_API_TOKEN),
+  VK_API_LIMIT: parseInt(checkEnv(Env.VK_API_LIMIT), 10),
+  VK_API_CALLBACK_RESPONSE: checkEnv(Env.VK_API_CALLBACK_RESPONSE),
+  VK_GROUP_ID: parseInt(checkEnv(Env.VK_GROUP_ID), 10),
+  MONGO_LOGIN: checkEnv(Env.MONGO_LOGIN),
+  MONGO_PASSWORD: checkEnv(Env.MONGO_PASSWORD),
+  MONGO_URI: checkEnv(Env.MONGO_URI),
 });
 
 export const config = initConfig();
